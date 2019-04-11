ed05fd72fd6299ab2175709e4c590bda4a5eccef
Le Tony <le.tony77340@gmail.com>
2019-04-11 16:49:10 +0200

    First GAME OK

---
+++ Tony/FizzBuzz/src/Game/main.java
@@ -0,0 +1,10 @@
+package Game;
+
+public class main {
+
+	
+	public static void main(String[]args) throws InterruptedException {
+		FizzBuzz fzbz = new FizzBuzz();
+		fzbz.LaunchGame();
+	}
+}
---
+++ Tony/FizzBuzz/src/test.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		   Scanner sc = new Scanner(System.in); 
+//		    System.out.println("Enter a number: ");
+//		    int limit = 3000;
+//		    long startTime = System.currentTimeMillis();
+//		    Long l = sc.nextLong();
+//		    if ((startTime + limit) < System.currentTimeMillis())
+//		        System.out.println("Sorry, your answer is too late");
+//		    else
+//		        System.out.println("Your answer is on time");
+		Scanner sc = new Scanner(System.in);
+		int limit = 3000;
+	    long startTime = System.currentTimeMillis();
+	    Long l = sc.nextLong();
+	    
+	    System.out.println(System.currentTimeMillis());
+	}
+
+}
