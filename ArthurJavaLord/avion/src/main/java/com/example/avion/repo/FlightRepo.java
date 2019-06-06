e0a611a5169f64fb3bad8f5ae89f961fbec4f1a6
Arthur <44750598+Andromed75@users.noreply.github.com>
2019-06-06 14:50:48 +0200

    ..

--- ArthurJavaLord/avion/src/main/java/com/example/avion/repo/FlightRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.avion.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.avion.model.Flight;
-
-public interface FlightRepo extends JpaRepository<Flight, Long> {
-
-}
--- ArthurJavaLord/avion/src/main/java/com/example/avion/repo/PiloteRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.avion.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.avion.model.Pilote;
-
-public interface PiloteRepo extends JpaRepository<Pilote, Long>{
-
-}
