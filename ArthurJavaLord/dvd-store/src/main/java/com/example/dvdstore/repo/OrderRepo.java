b989e6a03dd3d310b78f7b5d706957a6749616f2
Arthur <44750598+Andromed75@users.noreply.github.com>
2019-07-12 16:04:28 +0200

    rm rf 2

--- ArthurJavaLord/dvd-store/src/main/java/com/example/dvdstore/repo/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.dvdstore.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.dvdstore.entity.Order;
-
-public interface OrderRepo extends JpaRepository<Order, Long>{
-
-}
--- ArthurJavaLord/dvd-store/src/main/java/com/example/dvdstore/repo/PanierRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.dvdstore.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.dvdstore.entity.Panier;
-
-public interface PanierRepo extends JpaRepository<Panier, Long>{
-
-}
