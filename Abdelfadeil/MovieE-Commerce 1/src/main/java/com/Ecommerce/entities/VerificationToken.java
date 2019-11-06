0443913b31dc707011e667d4c6d00b9037f9957d
ABDELFADEIL <abdalfadeil@gmail.com>
2019-11-06 17:48:54 +0100

    mail verificaton class

---
+++ Abdelfadeil/MovieE-Commerce 1/src/main/java/com/Ecommerce/entities/VerificationToken.java
@@ -0,0 +1,39 @@
+package com.Ecommerce.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data @AllArgsConstructor @ToString
+public class VerificationToken implements Serializable {
+    public static final String  STATUS_PENDING = "PENDING";
+    public static final String  STATUS_VERIFIED = "VERIFIED";
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    private String status;
+    private LocalDateTime expiredDateTime;
+    private LocalDateTime issuedDateTime;
+    private LocalDateTime confirmedDateTime;
+    @OneToOne(cascade = CascadeType.ALL)
+    @Column(name = "user_id")
+    private AppUser user;
+
+    public VerificationToken(){
+        this.token = UUID.randomUUID().toString();
+        this.issuedDateTime = LocalDateTime.now();
+        this.expiredDateTime = this.issuedDateTime.plusDays(1);
+        this.status = STATUS_PENDING;
+    }
+
+
+
+}
---
+++ Abdelfadeil/MovieE-Commerce 1/src/main/java/com/Ecommerce/service/SendingMailService.java
@@ -0,0 +1,9 @@
+package com.Ecommerce.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendingMailService {
+
+
+}
